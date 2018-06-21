f7fd80f147d148e41a1ca66e7d2fdedaccbe92bf
Vikas <envikas@gmail.com>
2018-06-21 15:23:16 +1000

    Gets job links from all pages of dta

--- scraper.py
+++ scraper.py
@@ -2,13 +2,22 @@ from bs4 import BeautifulSoup
 import urllib.request as urllib2
 import re
 
+links = []
+
 # Getting all pagination links
 html_page = urllib2.urlopen("https://marketplace.service.gov.au/digital-marketplace/opportunities?page=1")
 soup = BeautifulSoup(html_page, "html5lib")
-links = []
+# Getting the last page number
+last_page_num = int(soup.find("div", class_="pagination").find("div").find_all("strong")[1].contents[0])
+print(last_page_num)
 
-# All the job titles are under a h2 heading, so getting all those links from the page
-for link in soup.select('h2 a[href]'):
-    links.append(link.get('href'))
+# Looping across all pages and getting links
+for i in range (0, last_page_num):
+    html_page = urllib2.urlopen("https://marketplace.service.gov.au/digital-marketplace/opportunities?page=" + str(i+1) )
+    soup = BeautifulSoup(html_page, "html5lib")
+    # All the job titles are under a h2 heading, so getting all those links from the page
+    for link in soup.select('h2 a[href]'):
+        links.append(link.get('href'))
+
 
 print(links)
