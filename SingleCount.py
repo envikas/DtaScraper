71fae22a11c6ccc6e71f9d9dffe11bf55baa0383
Vikas <envikas@gmail.com>
2018-07-25 17:58:58 +1000

    aDDED DATABASE TECH COUNTS

--- SingleCount.py
+++ SingleCount.py
@@ -57,10 +57,12 @@ for i in range (0, last_page_num):
 required_terms = ["oracle", "windows", "windows server", "macos", "mac os", "osx" "os x", "unix", "sql server", "swift", "javascript", "machine learning", "sql", "mysql", "nosql", "hadoop", "cobol", "foxtran", "r", "matlab", "tensorflow", "php", "spring framework", "pharmcis", "qi", "angularjs", "no-sql", "mainframe", "jira", "trello", "automation", "artificial intelligence" , "data mining", "big data", "ibm", "watson", ".net", "dotnet", "git", "agile", "lean", "fox", "database", "cms", "wordpress", "drupal", "azure", "aws", "linux", "android", "ios", "java" ,"c", "c++", "c#", "python", "ruby", "scala", "django", "html", "css", "react", "reactjs", "reactnative", "angular"]
 roles_terms = ["manager","business analyst", "engineer", "software engineer", "lawyer", "architect", "lead", "scrum master", "software developer", "programmer", "secretary"]
 platform_terms = ["aws","linux","azure", "ibm","microsoft server", "unix"]
+database_terms = ["my sql", "mysql","oracle", "sql server", "sqlserver", "db2", "sybase","hana", "postgresql", "postgre sql", "mariadb", "maria db", "teradata", "informic", "ingres", "simpledb"]
 resultList = sorted(terms.items(), key=operator.itemgetter(1), reverse=True)
 result = ""
 roles_result = ""
-platform_result =""
+platform_result =''
+database_result = ""
 
 for j in range(0,len(resultList)):
     if(resultList[j][1] > 1 and resultList[j][0].lower() in required_terms):
@@ -69,6 +71,8 @@ for j in range(0,len(resultList)):
         roles_result += resultList[j][0] + "," + str(resultList[j][1]) + "\n"
     if (resultList[j][1] > 1 and resultList[j][0].lower() in platform_terms):
         platform_result += resultList[j][0] + "," + str(resultList[j][1]) + "\n"
+    if (resultList[j][1] > 1 and resultList[j][0].lower() in database_terms):
+        database_result += resultList[j][0] + "," + str(resultList[j][1]) + "\n"
 
 # Get your department results in a CSV format
 departmentResultsList = sorted(department_frequency.items(), key=operator.itemgetter(1), reverse=True)
@@ -78,19 +82,22 @@ for j in range(0,len(departmentResultsList)):
         departmentResult += departmentResultsList[j][0] + "," + str(departmentResultsList[j][1]) + "\n"
 
 # Store the master text in file
-with io.open("Single_Count_aus.csv", "w", encoding="utf-8") as f:
+with io.open("Single_Count_uk.csv", "w", encoding="utf-8") as f:
     f.write(result)
     f.close()
 
-with io.open("Roles_Count_aus.csv", "w", encoding="utf-8") as f:
+with io.open("Roles_Count_uk.csv", "w", encoding="utf-8") as f:
     f.write(roles_result)
     f.close()
 
-with io.open("Platform_Count_aus.csv", "w", encoding="utf-8") as f:
+with io.open("Platform_Count_uk.csv", "w", encoding="utf-8") as f:
     f.write(platform_result)
     f.close()
 
+with io.open("Database_Count_uk.csv", "w", encoding="utf-8") as f:
+    f.write(database_result)
+    f.close()
 
-with io.open("Department_Frequency_aus.csv", "w", encoding="utf-8") as f:
+with io.open("Department_Frequency_uk.csv", "w", encoding="utf-8") as f:
     f.write(departmentResult)
     f.close()
